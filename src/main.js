f81268e5fdab1e4e7470911248fd2403973e7a84
Istvan Bodnar <istvan@gawker.com>
2016-01-15 16:29:24 +0100

    Base page with reflux

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
-import Hello from './hello.jsx';
-import World from './world.jsx';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+// import todoApp from './reducers'
+
+let store = createStore(() => {})
+
+let rootElement = document.getElementById('root')
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+)
--- src/world.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-class World extends React.Component {
-	render() {
-		return <h1>vilag</h1>
-	}
-}
-React.render(<World/>, document.getElementById('world'));
