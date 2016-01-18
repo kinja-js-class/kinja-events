a858977ced5418704e2ebc932378b37c3320bd79
Oliver Kovacs <oliver@kinja.com>
2016-01-18 10:18:46 +0100

    More tests

--- tests/Event.specs.js
+++ tests/Event.specs.js
@@ -1,5 +1,5 @@
-import { shallow } from 'enzyme';
-import sinon from 'sinon';
+import { shallow } from 'enzyme'
+import sinon from 'sinon'
 import { expect } from 'chai'
 
 import React from 'react'
@@ -8,31 +8,25 @@ import Event from '../src/components/Event'
 describe('<Event />', () => {
 
   it('renders one <li>', () => {
-    const wrapper = shallow(<Event onClick={() => {}} text="Hello Kinja!" completed={false} />);
-    expect(wrapper.find('li')).to.have.length(1);
-  });
+    const wrapper = shallow(<Event onClick={() => {}} text="Hello Kinja!" completed={false} />)
+    expect(wrapper.find('li')).to.have.length(1)
+  })
 
-  // it('renders an `.icon-star`', () => {
-  //   const wrapper = shallow(<MyComponent />);
-  //   expect(wrapper.find('.icon-star')).to.have.length(1);
-  // });
+  it('renders one <li> with the correct text', () => {
+    const wrapper = shallow(<Event onClick={() => {}} text="Hello Kinja!" completed={false} />)
+    expect(wrapper.find('li').text()).to.equal('Hello Kinja!')
+  })
 
-  // it('renders children when passed in', () => {
-  //   const wrapper = shallow(
-  //     <MyComponent>
-  //       <div className="unique" />
-  //     </MyComponent>
-  //   );
-  //   expect(wrapper.contains(<div className="unique" />)).to.equal(true);
-  // });
+  it('renders one <li> with the correct style', () => {
+    const wrapper = shallow(<Event onClick={() => {}} text="Hello Kinja!" completed={true} />)
+    expect(wrapper.find('li').hasClass('completed')).to.equal(true)
+  })
 
-  // it('simulates click events', () => {
-  //   const onButtonClick = sinon.spy();
-  //   const wrapper = shallow(
-  //     <Foo onButtonClick={onButtonClick} />
-  //   );
-  //   wrapper.find('button').simulate('click');
-  //   expect(onButtonClick.calledOnce).to.equal(true);
-  // });
+  it('simulates click events', () => {
+    const onButtonClick = sinon.spy()
+    const wrapper = shallow(<Event onClick={onButtonClick} text="Hello Kinja!" completed={false} />)
+    wrapper.simulate('click')
+    expect(onButtonClick.calledOnce).to.equal(true)
+  })
 
-});
+})
