77d4d9860d6d895a876fab03c5c4b57381cf2d73
Istvan Bodnar <istvan@gawker.com>
2016-03-30 22:53:32 +0200

    Sad attempts to make the dashboard look a bit more like a dashboard

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<AddCustomEvent />
-				<Paginator />
+				<div className="topbar">
+					<Paginator />
+					<AddCustomEvent />
+				</div>
 				<VisibleEventTimeline />
 				<VisibleEventStream />
 			</div>
